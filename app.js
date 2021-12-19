3132f98e5c1f8e182da550e77ca8cfff36f4e198
Ankur <faltuid24081997@gmail.com>
2021-12-19 20:57:46 +0530

    second commit

--- app.js
+++ app.js
@@ -135,6 +135,12 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
